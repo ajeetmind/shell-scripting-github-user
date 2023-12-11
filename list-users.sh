74c1da842a03ea77b838fb1be66e33164c3f924a
Ajeet Kumar <ajeet.kumar@finqle.com>
2023-12-11 19:41:34 +0100

    Script

---
+++ list-users.sh
@@ -0,0 +1,5 @@
+# This demo have sheel script and it will provide userlist
+```
+   This demo is providing all users having readonly access to Github at organization level.
+for auditing remove/add access at organization level 
+```
